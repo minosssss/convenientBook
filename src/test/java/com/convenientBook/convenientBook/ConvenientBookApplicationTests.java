822e86cf5a2c39d1a6111d6ff423ef1a60d8d75a
hoon <nice130@naver.com>
2024-01-14 14:57:12 +0900

    프로젝트 최초생성 2024-01-14

---
+++ src/test/java/com/convenientBook/convenientBook/ConvenientBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convenientBook.convenientBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenientBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
